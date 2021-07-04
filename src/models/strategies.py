0694cb37a9173b302c3dfa2838b53c7edf2c7559
richardgaus <richardgaus@outlook.de>
2021-07-04 18:33:37 +0200

    PNL history plot finished

--- src/models/strategies.py
+++ src/models/strategies.py
@@ -21,16 +21,96 @@ class StrategyResults(ABC):
     def __init__(self,
                  asset_name: str,
                  ohlcv_timeseries: pd.DataFrame,
+                 max_number_open_trades: int,
                  *args, **kwargs):
         self.asset_name = asset_name
         self.ohlcv = ohlcv_timeseries
+        self.max_number_open_trades = max_number_open_trades
         self.trades = []
+        self.eval_stats = {}
 
     def plot(self):
         pass
 
+    def plot_pnl_history(self,
+                         figsize: tuple = (12, 8),
+                         ylim: tuple = None):
+        fig, ax = mpl.pyplot.subplots(figsize=figsize)
+        ax.margins(x=0)
+        ax.plot(self.eval_stats['pnl_history'])
+        mpl.pyplot.ylim(ylim)
+        mpl.pyplot.xlabel("Open time")
+        mpl.pyplot.ylabel("Close PNL")
+        ax.yaxis.set_major_formatter(mpl.ticker.PercentFormatter(xmax=1.0))
+        mpl.pyplot.tight_layout()
+
+        mpl.pyplot.fill_between(
+            x=self.eval_stats['pnl_history'].index,
+            y1=self.eval_stats['pnl_history'], y2=0,
+            where=(self.eval_stats['pnl_history'] < 0),
+            alpha=0.30, color='red', interpolate=True
+        )
+        mpl.pyplot.fill_between(
+            x=self.eval_stats['pnl_history'].index,
+            y1=self.eval_stats['pnl_history'], y2=0,
+            where=(self.eval_stats['pnl_history'] >= 0),
+            alpha=0.30, color='green', interpolate=True
+        )
+        mpl.pyplot.show()
+
     def evaluation(self):
-        pass
+        trade_idx = 0
+        open_trades = []
+        budget = 1.0
+        total_value_series = []
+
+        for idx, row in self.ohlcv.iterrows():
+            # st = f'{idx} ' # Debug
+
+            # Update open positions
+            for ot in open_trades:
+                # Close due positions
+                if ot['exit_time'] == idx:
+                    if ot['long']:
+                        if ot['win']:
+                            gain = ot['take_profit'] / ot['entry_price']
+                        else:
+                            gain = ot['stop_loss'] / ot['entry_price']
+                    else:
+                        if ot['win']:
+                            gain = -ot['take_profit'] / ot['entry_price'] + 2
+                        else:
+                            gain = -ot['stop_loss'] / ot['entry_price'] + 2
+                    # st += f"close trade from {ot['entry_time']}. gain: {gain:.4f} " # Debug
+                    budget += gain * ot['investment']
+            open_trades = [ot for ot in open_trades if ot['exit_time'] > idx]
+
+            # Scan for trade entries
+            if trade_idx < len(self.trades) and self.trades[trade_idx]['entry_time'] == idx:
+                new_trade = self.trades[trade_idx].copy()
+                new_trade['investment'] = budget / (self.max_number_open_trades - len(open_trades))
+                budget -= new_trade['investment']
+                open_trades.append(new_trade)
+                trade_idx += 1
+                # st += f"open trade, invest {new_trade['investment']:.4f} " # Debug
+
+            # Update current value of all positions plus budget
+            total_value = budget
+            for ot in open_trades:
+                if ot['long']:
+                    val = row['close'] / ot['entry_price'] * ot['investment']
+                else:
+                    val = (-row['close'] / ot['entry_price'] + 2) * ot['investment']
+                total_value += val
+            total_value_series.append(total_value)
+            # st += f'budget: {budget:.4f}, total_value: {total_value:.4f}' # Debug
+            # print(st) # Debug
+
+        self.eval_stats['pnl_history'] = pd.Series(
+            data=total_value_series,
+            index=self.ohlcv.index
+        ) - 1.0
+        self.set_pnl(self.eval_stats['pnl_history'].iloc[-1])
 
     def set_pnl(self,
                 pnl: float):
@@ -41,8 +121,9 @@ class StrategyResults(ABC):
         return self._pnl
 
     def add_trade(self,
-                  entry_time,
+                  entry_time: pd.Timestamp,
                   entry_price: float,
+                  long: bool,
                   take_profit: float,
                   stop_loss: float,
                   exit_time,
@@ -50,6 +131,7 @@ class StrategyResults(ABC):
         _trade = {
             'entry_time': entry_time,
             'entry_price': entry_price,
+            'long': long,
             'take_profit': take_profit,
             'stop_loss': stop_loss,
             'exit_time': exit_time,
@@ -75,6 +157,7 @@ class BuyAndHold(Strategy):
             entry_time=ohlcv_timeseries.iloc[0].name,
             entry_price=entry,
             take_profit=exit,
+            long=True,
             stop_loss=exit,
             exit_time=ohlcv_timeseries.iloc[-1].name,
             win=exit > entry
@@ -89,37 +172,27 @@ class BuyAndHoldResults(StrategyResults):
                  ohlcv_timeseries: pd.DataFrame):
         super().__init__(
             asset_name=asset_name,
-            ohlcv_timeseries=ohlcv_timeseries
+            ohlcv_timeseries=ohlcv_timeseries,
+            max_number_open_trades=1
         )
-
-    def plot(self):
-        pass
-
-    def evaluation(self):
-        pnl = self.trades[0]['take_profit'] / self.trades[0]['entry_price'] - 1.0
-        self.set_pnl(pnl)
-
-    def set_pnl(self,
-                pnl: float):
-        self._pnl = pnl
-
-    @property
-    def pnl(self):
-        return self._pnl
 
 class RSIStoch200EMA(Strategy):
 
     def __init__(self,
-                 max_number_open_trades: int,
-                 min_period_to_last_low: int,
-                 max_period_to_last_low: int,
-                 min_absolute_slope_rsi: float,
-                 swing_low_margin: int):
+                 max_number_open_trades: int = 3,
+                 min_period_to_last_low: int = 5,
+                 max_period_to_last_low: int = 50,
+                 min_absolute_slope_rsi: float = 0.2,
+                 swing_low_margin: int = 2,
+                 swing_low_factor: float = 0.999,
+                 take_profit_factor: float = 2):
         super().__init__(max_number_open_trades)
         self.min_period_to_last_low = min_period_to_last_low
         self.max_period_to_last_low = max_period_to_last_low
         self.min_absolute_slope_rsi = min_absolute_slope_rsi
         self.swing_low_margin = swing_low_margin
+        self.swing_low_factor = swing_low_factor
+        self.take_profit_factor = take_profit_factor
 
     def apply(self,
               ohlcv_timeseries: pd.DataFrame,
@@ -249,15 +322,15 @@ class RSIStoch200EMA(Strategy):
                     current_trade = {
                         'entry_time': row.name,
                         'entry_price': row['close'],
+                        'long': True,
                         'take_profit': None,
                         'stop_loss': None,
                         'exit_time': None,
                         'win': None
                     }
                     # Price below EMA 200 -> go short; above EMA 200 -> go long
-                    go_long = True
                     if current_trade['entry_price'] < ohlcv_timeseries.iloc[idx]['ema']:
-                        go_long = False
+                        current_trade['long'] = False
                     # Look for nearest swing low/high
                     idx_swing_low = idx
                     while True:
@@ -266,48 +339,54 @@ class RSIStoch200EMA(Strategy):
                             range(-self.swing_low_margin, self.swing_low_margin + 1) if j != 0
                         ]
                         valid_idx = set(surrounding_idx).intersection(range(1, idx + 1))
-                        if go_long:  # go long
-                            if ohlcv_timeseries.iloc[idx_swing_low]['low'] < current_trade['entry_price'] \
+                        if current_trade['long']:  # go long
+                            if ohlcv_timeseries.iloc[idx_swing_low]['low'] < current_trade[
+                                'entry_price'] \
                                     and ohlcv_timeseries.iloc[idx_swing_low]['low'] < \
                                     ohlcv_timeseries.iloc[list(valid_idx)]['low'].min():
                                 # Swing low found. Place stop loss and take profit.
-                                current_trade['stop_loss'] = 0.999 * ohlcv_timeseries.iloc[idx_swing_low][
-                                    'low']
+                                current_trade['stop_loss'] = self.swing_low_factor * \
+                                    ohlcv_timeseries.iloc[idx_swing_low]['low']
+                                if current_trade['stop_loss'] > current_trade['entry_price']:
+                                    raise ValueError(
+                                        "swing_low_factor to high! Supply a lower value (preferrably < 1.0)."
+                                    )
                                 current_trade['take_profit'] = current_trade['entry_price'] \
-                                                               + 2 * (current_trade['entry_price'] -
-                                                                      current_trade['stop_loss'])
+                                    + self.take_profit_factor * (current_trade['entry_price'] -
+                                                                 current_trade['stop_loss'])
                                 break
                         else:  # go short
-                            if ohlcv_timeseries.iloc[idx_swing_low]['high'] > current_trade['entry_price'] \
+                            if ohlcv_timeseries.iloc[idx_swing_low]['high'] > current_trade[
+                                'entry_price'] \
                                     and ohlcv_timeseries.iloc[idx_swing_low]['high'] > \
                                     ohlcv_timeseries.iloc[list(valid_idx)]['high'].max():
                                 # Swing high found. Place stop loss and take profit.
-                                current_trade['stop_loss'] = 1.001 * ohlcv_timeseries.iloc[idx_swing_low][
-                                    'high']
+                                current_trade['stop_loss'] = (2 - self.swing_low_factor) * \
+                                    ohlcv_timeseries.iloc[idx_swing_low]['high']
+                                if current_trade['stop_loss'] < current_trade['entry_price']:
+                                    raise ValueError(
+                                        "swing_low_factor to high! Supply a lower value (preferrably < 1.0)."
+                                    )
                                 current_trade['take_profit'] = current_trade['entry_price'] \
-                                                               + 2 * (current_trade['entry_price'] -
-                                                                      current_trade['stop_loss'])
+                                    + self.take_profit_factor * (current_trade['entry_price'] -
+                                                                 current_trade['stop_loss'])
                                 break
                         idx_swing_low = idx_swing_low - 1
                     # Find trade exit
                     for idx_future in range(idx+1, len(ohlcv_timeseries) - 1):
-                        # Is trade long or short?
-                        long = True
-                        if current_trade['stop_loss'] > current_trade['take_profit']:
-                            long = False
-                        if (long and current_trade['stop_loss'] > ohlcv_timeseries.iloc[idx_future][
-                            'low']) or \
-                                (not long and current_trade['stop_loss'] <
-                                 ohlcv_timeseries.iloc[idx_future]['high']):
+                        if (current_trade['long'] and current_trade['stop_loss'] > \
+                            ohlcv_timeseries.iloc[idx_future]['low']) or \
+                            (not current_trade['long'] and current_trade['stop_loss'] <
+                            ohlcv_timeseries.iloc[idx_future]['high']):
                             # Stopp loss target hit
                             current_trade['exit_time'] = ohlcv_timeseries.iloc[idx_future].name
                             current_trade['win'] = False
                             open_trades_exit_times.append(idx_future)
                             break
-                        if (long and current_trade['take_profit'] < ohlcv_timeseries.iloc[idx_future][
-                            'high']) or \
-                                (not long and current_trade['take_profit'] >
-                                 ohlcv_timeseries.iloc[idx_future]['low']):
+                        if (current_trade['long'] and current_trade['take_profit'] < \
+                            ohlcv_timeseries.iloc[idx_future]['high']) or \
+                            (not current_trade['long'] and current_trade['take_profit'] >
+                            ohlcv_timeseries.iloc[idx_future]['low']):
                             # Take profit target hit
                             current_trade['exit_time'] = ohlcv_timeseries.iloc[idx_future].name
                             current_trade['win'] = True
@@ -329,14 +408,16 @@ class RSIStoch200EMAResults(StrategyResults):
                  asset_name: str,
                  ohlcv_timeseries: pd.DataFrame,
                  max_number_open_trades: int):
-        super().__init__(asset_name, ohlcv_timeseries)
-        self.max_number_open_trades = max_number_open_trades
+        super().__init__(asset_name, ohlcv_timeseries, max_number_open_trades)
         self.patterns = []
-        self.eval_stats = {}
 
     def plot(self,
              start_time: pd.Timestamp=None,
              end_time: pd.Timestamp=None):
+        if start_time is None:
+            start_time = self.ohlcv.iloc[0].name
+        if end_time is None:
+            end_time = self.ohlcv.iloc[-1].name
         data = self.ohlcv[start_time:end_time]
         fig, axes = mpl.pyplot.subplots(
             4, 1,
@@ -439,13 +520,14 @@ class RSIStoch200EMAResults(StrategyResults):
     def add_trade_and_signal(self,
                              entry_time: pd.Timestamp,
                              entry_price: float,
+                             long: bool,
                              take_profit: float,
                              stop_loss: float,
                              exit_time: pd.Timestamp,
                              win: bool,
                              divergence_timeframe: tuple):
         self.add_trade(
-            entry_time, entry_price, take_profit, stop_loss, exit_time, win
+            entry_time, entry_price, long, take_profit, stop_loss, exit_time, win
         )
         _signal = {
             'divergence_timeframe': divergence_timeframe
@@ -453,58 +535,9 @@ class RSIStoch200EMAResults(StrategyResults):
         self.patterns.append(_signal)
 
     def evaluation(self):
-        budget = 1.0
-        num_positions = 0
-        # Convert trades to chronology of buy/sell events
-        events = []
-        for t in self.trades:
-            # Calculate profit and loss of individual trade
-            if t['win']:
-                if t['entry_price'] > t['take_profit']:  # short trade
-                    pnl = - (t['take_profit'] / t['entry_price'] - 1.0)
-                else:  # long trade
-                    pnl = t['take_profit'] / t['entry_price'] - 1.0
-            else:
-                if t['entry_price'] < t['stop_loss']:  # short trade
-                    pnl = - (t['stop_loss'] / t['entry_price'] - 1.0)
-                else:  # long trade
-                    pnl = t['stop_loss'] / t['entry_price'] - 1.0
-            events.append({
-                'timestamp': t['entry_time'],
-                'event': 'entry',
-                'entry_time': t['entry_time'],
-                'exit_time': t['exit_time'],
-                'pnl': pnl,
-                'investment': np.NaN
-            })
-            events.append({
-                'timestamp': t['exit_time'],
-                'event': 'exit',
-                'entry_time': t['entry_time'],
-                'pnl': pnl
-            })
-            events_df = pd.DataFrame(events)
-            events_df = events_df.sort_values(
-                by=['timestamp', 'event'], ascending=[True, False]
-            ).reset_index()
+        super().evaluation()
 
-        pnl_history = pd.Series(index=self.ohlcv.index)
-        for idx, row in events_df.iterrows():
-            if row['event'] == 'entry':
-                investment = budget / (self.max_number_open_trades - num_positions)
-                events_df.loc[idx, 'investment'] = investment
-                budget -= investment
-                num_positions += 1
-            elif row['event'] == 'exit':
-                budget += events_df[
-                             (events_df['entry_time'] == row['entry_time']) & \
-                             (events_df['event'] == 'entry')
-                             ]['investment'].iloc[0] * (1.0 + row['pnl'])
-                num_positions -= 1
-
-        #self.set_pnl(total)
-        #self.eval_stats['pnl_history'] =
-
+        """
         data = self.ohlcv[start_time:end_time]
 
         days = end_time - start_time
@@ -580,3 +613,4 @@ class RSIStoch200EMAResults(StrategyResults):
         #print(float(win_05quant.to_numpy()))
         print(' Win quantile0.5:', float(win_05quant.to_numpy()),'\n','Win mean:', float(win_mean.to_numpy()),'\n','Loss quantile0.5:', float(loss_05quant.to_numpy()),'\n','Loss mean:', float(loss_mean.to_numpy()),'\n','Gain avg:', gain_avg,'\n',)
         print(' Buy&Hold', buy_hold,'\n','Days:',days.days,'\n','Total Trades:', total_trades,'\n','Winners:', winners,'\n','Losses:', losses,'\n','Win Percentage:', win_percentage,'\n','Max Wins:', max_wins,'\n','Max Losses:', max_losses,'\n','Overall Gain:', gain,'\n',)
+        """
